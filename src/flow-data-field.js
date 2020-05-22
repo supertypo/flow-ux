a1d7b7fcce3c037ce37317be26da1bb2f8ef65e4
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2020-05-21 21:09:42 -0400

    graph experiments

--- src/flow-data-field.js
+++ src/flow-data-field.js
@@ -175,10 +175,10 @@ export class FlowDataFieldGraph extends Flowd3Element {
 
 	getMargin(){
 		return {
-			bottom:20,
-			top:10,
-			left:100,
-			right:30
+			bottom:0,
+			top:0,
+			left:0,
+			right:0
 		}
 	}
 
@@ -221,17 +221,17 @@ export class FlowDataFieldGraph extends Flowd3Element {
 
 		const xAxis = g => g
 		.attr("transform", `translate(0,${height - margin.bottom})`)
-		.call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
+//		.call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
 
 		const yAxis = g => g
 		.attr("transform", `translate(${margin.left},0)`)
 		.call(d3.axisLeft(y))
 		.call(g => g.select(".domain").remove())
-		.call(g => g.select(".tick:last-of-type text").clone()
-			.attr("x", 3)
-			.attr("text-anchor", "start")
-			.attr("font-weight", "bold")
-			.text(this.title));
+		// .call(g => g.select(".tick:last-of-type text").clone()
+		// 	.attr("x", 3)
+		// 	.attr("text-anchor", "start")
+		// 	.attr("font-weight", "bold")
+		// 	.text(this.title));
 
 		const area = d3.area()
 			.curve(d3.curveLinear)
