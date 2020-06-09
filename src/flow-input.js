4cce074fc83dcf0413dfb8065fd06f6a2c2c3f63
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2020-06-08 23:45:55 -0400

    flow input label margin

--- src/flow-input.js
+++ src/flow-input.js
@@ -66,7 +66,7 @@ export class FlowInput extends BaseElement {
 				font-size:0.7rem;padding:2px 5px;
 				border:2px solid var(--flow-border-color, var(--flow-primary-color, rgba(0,151,115,1)));
 				border-radius:8px;
-    			margin-left:16px;z-index:1;
+    			margin-left:10px;z-index:1;
     			position:relative;background-color:var(--flow-input-bg, inherit);
 			}
 			.btn{
