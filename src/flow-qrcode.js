151e98214eaf460fd7346b74bb983be2b043cca0
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2020-07-02 15:03:06 -0400

    working on qrcode

--- src/flow-qrcode.js
+++ src/flow-qrcode.js
@@ -1,4 +1,5 @@
 import {BaseElement, html, css, baseUrl} from './base-element.js';
+import { dpc } from './helpers.js';
 
 if(!window.__FLOW__QRCODE__){
 	let qrcode = document.createElement("script");
@@ -22,7 +23,11 @@ export class FlowQRCode extends BaseElement {
 	static get properties() {
 		return {
 			for : { type : String },
-			type : { type : String }
+			ntype : { type : Number },
+			data : { type : String },
+			ecl : { type : String },
+			mode : { type : String },
+			multibyte : { type : String },	
 		}
 	}
 
@@ -30,23 +35,64 @@ export class FlowQRCode extends BaseElement {
 		return css`
 			:host {
 				display : block;
+/*				min-width: 32px;
+				min-height: 32px;
+*/				
 			}
 		`;
 	}
 
 	constructor() {
 		super();
+
+		this.ntype = 0;
+		this.ecl = 'M';
+		this.mode = 'Byte';
+		this.multibyte = 'UTF-8';
+		this.data = '';
 	}
 
 	render() {
+
+		if(!window.qrcode) {
+			dpc(128, () => {
+				this.requestUpdate();
+			});
+			return 'QR';
+		}
+
 		if(!this.qr || this.data != this.data_last_) {
 //			this.qrdata = this.qrcode.createDataURL(width,height,getPixel)
 //			this.qr = this.create_qrcode(this.data);
+			
 			this.data_last_ = this.data;
+
+			let qr = this.createQRCode(this.data, this.ntype, this.ecl, this.mode, this.multibyte);
+
+
+			let template = document.createElement('template');
+			template.innerHTML = qr.trim();
+			this.qr = template.content.firstChild.cloneNode(true);
+		//this.svg = template.content.firstChild.cloneNode(true);
+
 		}
-		return html`${this.qrdata}`;
+		return this.qr; //html`${this.qr}`;
 	}
 
+	createQRCode(text, typeNumber,
+		errorCorrectionLevel, mode, mb) {
+	
+	  window.qrcode.stringToBytes = window.qrcode.stringToBytesFuncs[mb];
+	
+	  var qr = qrcode(typeNumber || 4, errorCorrectionLevel || 'M');
+	  qr.addData(text, mode);
+	  qr.make();
+	
+	//  return qr.createTableTag();
+	//  return qr.createSvgTag();
+	  return qr.createImgTag(5);
+	};
+	
 
 /*
 
@@ -56,19 +102,6 @@ var draw_qrcode = function(text, typeNumber, errorCorrectionLevel) {
   document.write(create_qrcode(text, typeNumber, errorCorrectionLevel) );
 };
 
-var create_qrcode = function(text, typeNumber,
-    errorCorrectionLevel, mode, mb) {
-
-  qrcode.stringToBytes = qrcode.stringToBytesFuncs[mb];
-
-  var qr = qrcode(typeNumber || 4, errorCorrectionLevel || 'M');
-  qr.addData(text, mode);
-  qr.make();
-
-//  return qr.createTableTag();
-//  return qr.createSvgTag();
-  return qr.createImgTag();
-};
 
 var update_qrcode = function() {
   var form = document.forms['qrForm'];
