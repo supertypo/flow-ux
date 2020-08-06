db5eee017213e694f2b248728d3485194c51bbcb
surinder singh <surinder83singh@gmail.com>
2020-08-06 07:11:51 +0530

    FlowFormControl [expandable]

--- src/flow-form-control.js
+++ src/flow-form-control.js
@@ -16,7 +16,10 @@ import {BaseElement, html, css} from './base-element.js';
 export class FlowFormControl extends BaseElement {
 	static get properties() {
 		return {
-			icon:{type: String}
+			icon:{type: String},
+			expandIcon:{type: String},
+			expandable:{type:Boolean},
+			expand:{type:Boolean, reflect:true}
 		}
 	}
 
@@ -27,26 +30,32 @@ export class FlowFormControl extends BaseElement {
 			align-items:flex-start;
 			margin:10px 0px;
 		}
-		.icon-box{
+		.icon-box,
+		.expandable-icon-box{
 			width:30px;
 			max-width:30px;
 			text-align:center;
 		}
+		.expandable-icon-box svg{cursor:pointer}
 		:host([icon="none"]) .icon-box{display:none}
 		:host(.no-icon]) .icon-box{display:none}
 		:host([no-icon]) .icon-box{display:none}
 
-		.icon-box svg{
+		.icon-box svg,
+		.expandable-icon-box svg{
 			width:24px;
 			height:24px;
 			margin-right: 8px;
 			fill:var(--flow-primary-color, rgba(0,151,115,1.0));
 		}
+		.title-box{
+			user-select:none;line-height:24px;cursor:pointer;
+		}
 		.input-box{
 			width: var(--flow-form-control-input-box-width,100px);
 			flex:1;
 		}
-		.input{margin:5px 5px 5px 10px;}
+		.input{margin:5px 5px 5px 0px;}
 		:host([no-help]) .info-box,
 		:host([no-info]) .info-box{
 			display:none;
@@ -68,28 +77,62 @@ export class FlowFormControl extends BaseElement {
 		.info-box ::slotted(p){
 			font-size:0.8em;
 		}
-		/*
-		.input-box ::slotted(flow-folder-input){
-			--flow-folder-input-width:100%;
-			--flow-folder-input-min-width:100px;
-			--flow-folder-input-max-width:500px;
+		:host([expandable]:not([expand])) .input,
+		:host([expandable]:not([expand])) .info-box ::slotted(*){
+			display:none;
 		}
-		*/
+		:host([expand]:not([static-icon])) .expandable-icon-box svg{
+			transform:rotate(90deg)
+		}
 		`;
 	}
 	render() {
 		let iconSrc = "";
 		if(this.icon != "-")
 			iconSrc = this.iconPath(this.icon || "info-circle");
+		let icon2Src = "";
+		if(this.expandIcon != "-")
+			icon2Src = this.iconPath(this.expandIcon || "caret-right");
 		return html`
 			<div class="icon-box"><svg><use href="${iconSrc}"></use></svg></div>
+			${
+				this.expandable?
+				html`<div class="expandable-icon-box" 
+					data-flow-expandable="toggle"><svg>
+				<use href="${icon2Src}"></use>
+				</svg></div>`: ''
+			}
 			<div class="input-box">
-				<label class="title-box"><slot name="title"></slot></label>
+				<label class="title-box" 
+					data-flow-expandable="toggle"><slot name="title"></slot></label>
 				<div class="input"><slot></slot><slot name="input"></slot></div>
 			</div>
 			<div class="info-box"><slot name="info"></slot></div>
 		`;
 
+	}
+	firstUpdated(){
+		this.renderRoot.addEventListener("click", this._onClick.bind(this));
+	}
+
+	_onClick(e){
+		let target = e.target.closest("[data-flow-expandable]")
+		if(!target)
+			return
+		let action = target.getAttribute("data-flow-expandable") || 'toggle';
+
+		if(["toggle", "open", "close"].includes(action))
+			this[action]();
+	}
+
+	open(){
+		this.expand = true;
+	}
+	close(){
+		this.expand = false;
+	}
+	toggle(){
+		this.expand = !this.expand;
 	}
 }
 
