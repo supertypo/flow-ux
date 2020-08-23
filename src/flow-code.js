2d0b65ebeeacddfaab190b15c3e0f98a73267f60
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2020-08-23 05:33:28 -0400

    innerHTML instead of textarea

--- src/flow-code.js
+++ src/flow-code.js
@@ -57,7 +57,8 @@ export class FlowCode extends BaseElement {
 	render() {
 		//let indent = this.clcIndent();
 		if (!this.innerHTML_) {
-			let v = this.querySelector("textarea").value;
+//			let v = this.querySelector("textarea").value;
+			let v = this.innerHTML; //querySelector("textarea").value;
 			v = v.split("\n").map(v => {
 				//console.log("v1", v)
 				// why was this here? this [\t ]* is breaking code...
