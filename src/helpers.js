f820117bc42f583caad440d982a41fcc9bdc1421
surinder singh <surinder83singh@gmail.com>
2020-11-23 07:00:09 +0530

    getRandomInt, shuffle, chunks helpers

--- src/helpers.js
+++ src/helpers.js
@@ -447,3 +447,29 @@ export const buildReCaptcha = root=>{
 		});
 	})
 }
+
+export const chunks = (arr, size) =>{
+	return arr.length ? [arr.slice(0, size), ...chunks(arr.slice(size), size)] : []
+}
+export const getRandomInt = (min, max)=>{
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max + 1 - min) + min); //The maximum and minimum are inclusive
+}
+export const shuffle = (array)=>{
+	let currentIndex = array.length, temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
