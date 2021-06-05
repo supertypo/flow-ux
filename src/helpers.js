6df34f471078eff087c7b46b2337840a267e05db
surinder singh <surinder83singh@gmail.com>
2021-06-05 16:16:10 +0530

    abbreviateNumber

--- src/helpers.js
+++ src/helpers.js
@@ -365,6 +365,19 @@ export const getRandomInt = (min, max)=>{
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max + 1 - min) + min); //The maximum and minimum are inclusive
 }
+
+export const abbreviateNumber = (num, fixed)=>{
+	if (num === null) { return null; } // terminate early
+	if (num === 0) { return '0'; } // terminate early
+	fixed = (!fixed || fixed < 0) ? 2 : fixed; // number of decimal places to show
+	var b = (num).toPrecision(2).split("e"), // get power
+		k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
+		c = k < 1 ? num.toFixed(0 + fixed) : (num / Math.pow(10, k * 3) ).toFixed(1 + fixed), // divide by power
+		d = c < 0 ? c : Math.abs(c), // enforce -0 is 0
+		e = d + ['', 'K', 'M', 'B', 'T'][k]; // append power
+	return e;
+}
+
 export const shuffle = (array)=>{
 	let currentIndex = array.length, temporaryValue, randomIndex;
 
